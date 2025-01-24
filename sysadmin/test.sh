caca981323131d71fcbbd21debed4d8109aed439
Sam Nunn <sam@nunn.io>
2025-01-24 14:22:12 +0800

    Remove automatic start/stop of containers from test.sh

--- sysadmin/test.sh
+++ sysadmin/test.sh
@@ -4,7 +4,7 @@ echo "Downloading test browsers..."
 pipenv run playwright install chromium webkit
 
 echo "Starting containers..."
-docker compose -f compose.test.yaml up -d
+# docker compose -f compose.test.yaml up -d
 
 # echo "Waiting for server to be ready..."
 # sleep 10
@@ -13,4 +13,4 @@ echo "Running tests..."
 pipenv run pytest --browser webkit --browser chromium tests/ -v
 
 echo "Cleaning up..."
-docker compose -f compose.test.yaml down
+# docker compose -f compose.test.yaml down
