8a856df38c1868d106870691536bf2d2e89ec75e
Sam Nunn <sam@nunn.io>
2025-01-24 15:08:46 +0800

    Adds a test for CMD-K interface

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -15,3 +15,10 @@ def test_popover(page):
     page.get_by_role("button", name="Accept").click()
     page.locator("[popovertarget]").first.click()
     expect(page.locator("dialog.clinic-modal-popup-dialog").first).to_be_visible()
+
+
+def test_cmd_k(page):
+    page.goto("http://127.0.0.1:8070/clinic")
+    page.get_by_role("button", name="Accept").click()
+    page.keyboard.press("Meta+k")
+    expect(page.locator("#quick-find-input")).to_be_focused()
