ec83d5122f3ed95c45c593664653cc0bc836007e
Sam Nunn <sam@nunn.io>
2025-01-24 14:29:34 +0800

    Re-enable CMD-K

--- www/static/js/components/cmd-k.js
+++ www/static/js/components/cmd-k.js
@@ -3,51 +3,51 @@
 //   | |___| |  | | |_| |_____| . \                                                
 //    \____|_|  |_|____/      |_|\_\                                               
 
-// window.addEventListener('load', (e) => {
-//     let allInputs = document.querySelectorAll('clinic-input')
-//     document.allInputs = []
-//     for (let i of allInputs) {
-//         if (!i.data.search_name) continue
-//         document.allInputs.push({
-//             'name': i.data.search_name,
-//             'element': i,
-//         })
-//     }
-// })
+window.addEventListener('load', (e) => {
+    let allInputs = document.querySelectorAll('clinic-input')
+    document.allInputs = []
+    for (let i of allInputs) {
+        if (!i.data.search_name) continue
+        document.allInputs.push({
+            'name': i.data.search_name,
+            'element': i,
+        })
+    }
+})
 
-// let quickFindDialog = document.querySelector('#quick-find')
-// let quickFindSearch = document.querySelector('#quick-find-input')
-// let quickFindResults = document.querySelector('#quick-find-results')
-// document.addEventListener("keydown", (e) => {
-//     let key = e.key.toLowerCase()
-//     if ((key === "k" && (e.metaKey || e.ctrlKey))) {
-//         e.preventDefault()
-//         quickFindDialog.showModal()
-//     }
-// })
-// quickFindSearch.addEventListener('input', (e) => {
-//     let results = fuzzysort.go(e.target.value, document.allInputs, {key: 'name', limit: 5})
-//     quickFindResults.innerHTML = ''
-//     for (let r of results) {
-//         let li = document.createElement('li')
-//         li.innerHTML = `${r.obj['name']}<button tabindex="2">Go</button>`
-//         li.onclick = (e) => {
-//             // scroll into view and focus
-//             // setTimeout() seems to be required here
-//             r.obj['element'].scrollIntoView({ block: "center", inline: "nearest"})
-//             setTimeout(() => { r.obj['element'].focus() }, 0)
-//             // be gone
-//             quickFindDialog.close()
-//             // reset
-//             quickFindSearch.value = ''
-//             quickFindResults.innerHTML = ''
-//         }
-//         quickFindResults.appendChild(li)
-//         quickFindResults.firstChild.setAttribute('aria-selected', 'true')
-//     }
-// })
+let quickFindDialog = document.querySelector('#quick-find')
+let quickFindSearch = document.querySelector('#quick-find-input')
+let quickFindResults = document.querySelector('#quick-find-results')
+document.addEventListener("keydown", (e) => {
+    let key = e.key.toLowerCase()
+    if ((key === "k" && (e.metaKey || e.ctrlKey))) {
+        e.preventDefault()
+        quickFindDialog.showModal()
+    }
+})
+quickFindSearch.addEventListener('input', (e) => {
+    let results = fuzzysort.go(e.target.value, document.allInputs, {key: 'name', limit: 5})
+    quickFindResults.innerHTML = ''
+    for (let r of results) {
+        let li = document.createElement('li')
+        li.innerHTML = `${r.obj['name']}<button tabindex="2">Go</button>`
+        li.onclick = (e) => {
+            // scroll into view and focus
+            // setTimeout() seems to be required here
+            r.obj['element'].scrollIntoView({ block: "center", inline: "nearest"})
+            setTimeout(() => { r.obj['element'].focus() }, 0)
+            // be gone
+            quickFindDialog.close()
+            // reset
+            quickFindSearch.value = ''
+            quickFindResults.innerHTML = ''
+        }
+        quickFindResults.appendChild(li)
+        quickFindResults.firstChild.setAttribute('aria-selected', 'true')
+    }
+})
 
 
-// document.querySelector('#quick-find-button')?.addEventListener('click', (e) => {
-//     quickFindDialog.showModal()
-// })
+document.querySelector('#quick-find-button')?.addEventListener('click', (e) => {
+    quickFindDialog.showModal()
+})
