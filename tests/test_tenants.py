b94c70a7a497607b06331a368f143c58d4379c30
Sam Nunn <sam@nunn.io>
2025-01-30 21:31:49 +0800

    Update tenant tests for new UI

--- tests/test_tenants.py
+++ tests/test_tenants.py
@@ -6,12 +6,16 @@ def test_tenant_authentication(page: Page):
     page.goto("http://127.0.0.1:8070/clinic")
     page.get_by_role("button", name="Accept").click()
     utils.tenant_sign_in(page)
-    expect(page.get_by_role("button", name="Connect 🟢")).to_be_visible()
+    expect(page.get_by_role("button", name="Connected 🟢")).to_be_visible()
 
 
 def test_tenant_deauthentication(page: Page):
     page.goto("http://127.0.0.1:8070/clinic")
     page.get_by_role("button", name="Accept").click()
     utils.tenant_sign_in(page)
+    page.goto("http://127.0.0.1:8070/clinic")
     utils.tenant_sign_out(page)
-    expect(page.get_by_role("button", name="Connect 🟢")).not_to_be_visible()
+    page.goto("http://127.0.0.1:8070/clinic")
+    page.main_frame.wait_for_load_state()
+    page.wait_for_timeout(100)
+    expect(page.get_by_role("button", name="Connected 🟢")).not_to_be_visible()
