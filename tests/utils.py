39636250e52a544b3acc2f9e287078627faea117
Sam Nunn <sam@nunn.io>
2025-01-30 21:32:19 +0800

    Update test utilities for new UI

--- tests/utils.py
+++ tests/utils.py
@@ -14,8 +14,10 @@ def tenant_sign_in(page):
     page.get_by_placeholder("Hospital ID").click()
     page.get_by_placeholder("Hospital ID").fill(get_test_tentant_id())
     page.get_by_role("button", name="Connect", exact=True).click()
+    page.main_frame.wait_for_load_state()
 
 
 def tenant_sign_out(page):
-    page.get_by_role("button", name="Connect 🟢").click()
+    page.get_by_role("button", name="Connected 🟢").click()
     page.get_by_role("button", name="Disconnect").click()
+    page.main_frame.wait_for_load_state()
