cd448ad422b56e753c199d39d05946d01a444200
Sam Nunn <sam@nunn.io>
2025-02-17 21:01:05 +0800

    Create PeriscopeDataType model

--- www/clinic/admin.py
+++ www/clinic/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import PeriscopeDataType
+
+
 # Register your models here.
+@admin.register(PeriscopeDataType)
+class PeriscopeDataTypeAdmin(admin.ModelAdmin):
+    list_display = ["ugly_name"]
+    pass
--- www/clinic/models.py
+++ www/clinic/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class PeriscopeDataType(models.Model):
+    ugly_name = models.CharField(max_length=200, blank=False, unique=True)
+    pretty_name = models.CharField(max_length=200, blank=False)
+    search_name = models.CharField(max_length=200, blank=True)
+
+    output_prefix = models.CharField(max_length=200, blank=True)
+    output_suffix = models.CharField(max_length=200, blank=True)
+
+    patient_facing_label = models.CharField(max_length=200, blank=True)
+    patient_facing_description = models.TextField(blank=True)
+
+    clinician_facing_label = models.CharField(max_length=200, blank=True)
+    clinician_facing_description = models.TextField(blank=True)
+
+    html = models.TextField(blank=False)
