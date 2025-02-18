29a7badef7f6ab1b39aceb07cf9dde7d7c951fa0
Sam Nunn <sam@nunn.io>
2025-02-18 17:17:21 +0800

    Disable client-side <clinic-input> hydration

--- www/clinic/static/js/clinic-inputs.js
+++ www/clinic/static/js/clinic-inputs.js
@@ -17,37 +17,36 @@ customElements.define('clinic-input', class extends HTMLElement {
         this.label_enabled = this.getAttribute("label") != "false" // true by default
         this.data = allInputs.find((i) => i.ugly_name == this.ugly_name)
 
-        // boilerplate
-        this.setAttribute("autocomplete", "off")
-
         // insert HTML
-        let html
-        if (this.label_enabled) {
-            if (this.data.citation) {
-                html = `
-                <label>
-                    <span>
-                        ${this.data.pretty_name}
-                        <clinic-modal-popup>
-                            <button class="infobutton" tabindex="-1"></button>
-                            <template>
-                                <clinic-citation-snippet citation-id="${this.data.citation}"></clinic-citation-snippet>
-                            </template>
-                        </clinic-modal-popup>
-                    </span>
-                    ${this.data.html}
-                </label>`
-            } else {
-                html = `
-                <label>
-                    ${this.data.pretty_name}
-                    ${this.data.html}
-                </label>`
-            }
-        } else {
-            html = this.data.html
-        }
-        this.innerHTML = html
+        // if (!this.innerHTML) {
+        //     let html
+        //     if (this.label_enabled) {
+        //         if (this.data.citation) {
+        //             html = `
+        //             <label>
+        //                 <span>
+        //                     ${this.data.pretty_name}
+        //                     <clinic-modal-popup>
+        //                         <button class="infobutton" tabindex="-1"></button>
+        //                         <template>
+        //                             <clinic-citation-snippet citation-id="${this.data.citation}"></clinic-citation-snippet>
+        //                         </template>
+        //                     </clinic-modal-popup>
+        //                 </span>
+        //                 ${this.data.html}
+        //             </label>`
+        //         } else {
+        //             html = `
+        //             <label>
+        //                 ${this.data.pretty_name}
+        //                 ${this.data.html}
+        //             </label>`
+        //         }
+        //     } else {
+        //         html = this.data.html
+        //     }
+        //     this.innerHTML = html
+        // }
 
         // identify input element
         this.inputElement = this.querySelector("input, select, textarea")
