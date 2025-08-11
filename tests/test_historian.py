f36bed2723b1e0cca4eb67e7742e7144c56fa3cd
Sam Nunn <sam@nunn.io>
2025-08-11 11:02:23 +0930

    Add tests for custom diagnosis adding and persistence

--- tests/test_historian.py
+++ tests/test_historian.py
@@ -1,9 +1,10 @@
-from playwright.sync_api import Page, expect
-from utils import run_preamble
 import re
 
+from playwright.sync_api import Page, expect
+from utils import run_preamble
 
-def test_diagnosis_adding(page):
+
+def test_diagnosis_adding(page: Page):
     run_preamble(page)
     page.get_by_placeholder("Search for a condition").click()
     page.get_by_placeholder("Search for a condition").fill("t2d")
@@ -11,3 +12,27 @@ def test_diagnosis_adding(page):
     expect(
         page.locator('clinic-diagnosis[clinic-parameter="diagnosis-t2dm"]')
     ).to_be_visible()
+
+
+def test_custom_diagnosis_adding(page: Page):
+    run_preamble(page)
+
+    page.get_by_placeholder("Search for a condition").click()
+    page.get_by_placeholder("Search for a condition").fill("rhubarbosis")
+
+    page.get_by_role("listitem").filter(has_text="rhubarbosis").click()
+
+    expect(page.locator("#diagnosis-list input")).to_have_value("rhubarbosis")
+
+
+def test_custom_diagnosis_persistence(page: Page):
+    run_preamble(page)
+
+    page.get_by_placeholder("Search for a condition").click()
+    page.get_by_placeholder("Search for a condition").fill("rhubarbosis")
+
+    page.get_by_role("listitem").filter(has_text="rhubarbosis").click()
+
+    page.reload()
+
+    expect(page.locator("#diagnosis-list input")).to_have_value("rhubarbosis")
